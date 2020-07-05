964e96dd54d6223ad0a24ca045cc9241871a3faf
dkhamsing <dkhamsing8@gmail.com>
2020-07-05 07:56:46 -0700

    Add SwiftUI Playground

---
+++ 5-swiftui-playground/listapp-playground.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    @State var shows: [Show] = []
+
+    var body: some View {
+        NavigationView {
+            List(shows) { show in
+                VStack(alignment: .leading) {
+                    Text(show.name)
+                    Text(show.subtitle)
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                        .lineLimit(4)
+                }
+            }
+            .navigationBarTitle("SwiftUI - iOS 13 - Playground", displayMode: .inline)
+            .onAppear {
+                self.loadData()
+            }
+        }
+    }
+
+    func loadData() {
+        let urlString = "https://api.tvmaze.com/shows"
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let data = data,
+                let shows = try? JSONDecoder().decode([Show].self, from: data) else { return }
+
+            DispatchQueue.main.async {
+                self.shows = shows
+            }
+        }.resume()
+    }
+}
+
+struct Show: Codable, Identifiable {
+    let id: Int
+    let name, status, premiered, summary: String
+}
+
+extension Show {
+    var subtitle: String {
+        return premiered + "\n" + status + summary
+            .replacingOccurrences(of: "<p>", with: "\n")
+            .replacingOccurrences(of: "</p>", with: "\n")
+            .replacingOccurrences(of: "<b>", with: "")
+            .replacingOccurrences(of: "</b>", with: "")
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Basic lists on iOS :sparkles:
 - [UICollectionView](2-uicollectionview/) `iOS 6` `2012` [`diff`](https://github.com/dkhamsing/listapp.ios/commit/6b114564bd68ac883cc89258a2641007a756b49b)
 - [Compositional Layout](3-compositional-layout/) `iOS 13` `2019` [`diff`](https://github.com/dkhamsing/listapp.ios/commit/eba3f87bf3ec5c9703a5b0db0c01cb4d2d4ab582)
 - [Diffable Data Source](4-diffable-data-source/) `iOS 13` `2019` [`diff`](https://github.com/dkhamsing/listapp.ios/commit/a624a91b6e91227bc8798e50f99a081d5eb56cba)
-- [SwiftUI](5-swiftui/) `iOS 13` `2019` [`diff`](https://github.com/dkhamsing/listapp.ios/commit/3ee5b2688979d1141f09b0046be96f31a64b2116)
+- [SwiftUI](5-swiftui/) `iOS 13` `2019` [`Playground`](5-swiftui-playground/)  [`diff`](https://github.com/dkhamsing/listapp.ios/commit/3ee5b2688979d1141f09b0046be96f31a64b2116)
 - [UICollectionView Compositional Layout List](6-uicollectionview-compositional-layout-list) `iOS 14` `2020` [`diff`](https://github.com/dkhamsing/listapp.ios/commit/26e78fcbeb3c48e3c65c9354150d9f7ecdfd90a7)
 
 <img src=images/1.png width=600>
